2743cf1ae729739ba6ebad37ead5e9276671cc8e
LaptevaKate <ekaterina213@yandex.ru>
2023-12-27 12:13:32 +0300

    first commit

---
+++ SimpleCardInfoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCardInfoApp
+//
+//  Created by Екатерина Лаптева on 27.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
